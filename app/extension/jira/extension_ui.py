12ca3102282eebb15e70c6e20deac14e36b59b14
Dzmitry_Asonau <dzmitry_asonau@epam.com>
2024-09-26 15:29:56 +0300

    Remove sub-measures

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -16,34 +16,33 @@ create_project_option_id = 'com.epam.ework.jira.ework-integration-qa:ework-proje
 issue_without_ework_project_key = 'DT-14'
 issue_with_ework_project_key = 'DT-48'
 ework_project_with_task = 'AUTO_STREAM'
+wait_timeout = 5
+small_wait_timeout = 50
+number_of_attempts = 10
 
 
 def check_edit_project(webdriver):
     page = BasePage(webdriver)
 
-    @print_timing("selenium_edit_project")
+    @print_timing("selenium_view_open_project_option")
     def measure():
-        @print_timing("selenium_edit_project:view_open_project_option")
-        def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_with_ework_project_key}")
-            page.wait_until_visible((By.ID, "summary-val"))
-            page.wait_until_visible((By.ID, "opsbar-operations_more")).click()
-            page.wait_until_present(
-                (By.ID, "com.epam.ework.jira.ework-integration-qa:ework-project-button_preview")).click()
+        page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_with_ework_project_key}")
+        page.wait_until_visible((By.ID, "summary-val"))
+        page.wait_until_visible((By.ID, "opsbar-operations_more")).click()
+        page.wait_until_present(
+            (By.ID, "com.epam.ework.jira.ework-integration-qa:ework-project-button_preview")).click()
 
-        sub_measure()
+    measure()
 
-        @print_timing("selenium_edit_project:edit_project_requirements")
-        def sub_measure():
-            page.wait_until_visible((By.XPATH, "//span[text()='Edit project']")).click()
-            page.wait_until_visible((By.XPATH, "//input[@name='name']"))
-            page.wait_until_visible((By.XPATH, "//div[text()='Instructions']")).click()
-            page.wait_until_present((By.XPATH, "//trix-editor[@input='react_trix-editor_requirements']")).send_keys(
-                "update")
-            page.wait_until_visible((By.XPATH, "//span[text()='Save changes']")).click()
-            page.wait_until_visible((By.XPATH, "//span[text()='Close Page']"))
-
-        sub_measure()
+    @print_timing("selenium_edit_project_requirements")
+    def measure():
+        page.wait_until_visible((By.XPATH, "//span[text()='Edit project']")).click()
+        page.wait_until_visible((By.XPATH, "//input[@name='name']"))
+        page.wait_until_visible((By.XPATH, "//div[text()='Instructions']")).click()
+        page.wait_until_present((By.XPATH, "//trix-editor[@input='react_trix-editor_requirements']")).send_keys(
+            "update")
+        page.wait_until_visible((By.XPATH, "//span[text()='Save changes']")).click()
+        page.wait_until_visible((By.XPATH, "//span[text()='Close Page']"))
 
     measure()
 
@@ -51,7 +50,7 @@ def check_edit_project(webdriver):
 def check_create_project_option(webdriver):
     page = BasePage(webdriver)
 
-    @print_timing("selenium_view_issue:view_create_project_option")
+    @print_timing("selenium_view_create_project_option")
     def measure():
         page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_without_ework_project_key}")
         page.wait_until_visible((By.ID, "summary-val"))
@@ -75,83 +74,91 @@ def check_project_creation(webdriver):
 
     page = BasePage(webdriver)
 
-    @print_timing("selenium_create_project")
+    @print_timing("selenium_create_project:select_project_creation_option")
     def measure():
-        @print_timing("selenium_create_project:select_project_creation_option")
-        def sub_measure():
-            page.wait_until_present((By.XPATH, "//a[@class='issue-created-key issue-link']")).click()
-            page.wait_until_present((By.ID, "opsbar-operations_more")).click()
-            page.wait_until_visible((By.ID, create_project_option_id)).click()
+        page.wait_until_present((By.XPATH, "//a[@class='issue-created-key issue-link']")).click()
+        page.wait_until_present((By.ID, "opsbar-operations_more")).click()
+        page.wait_until_visible((By.ID, create_project_option_id)).click()
 
-        sub_measure()
+    measure()
 
-        next_step = "//span[text()='Continue']"
+    next_step = "//span[text()='Continue']"
 
-        @print_timing("selenium_create_project:select_create_ework_project")
-        def sub_measure():
-            page.wait_until_visible((By.XPATH, "//div[text()='Create a Single Task']/../..")).click()
-            page.wait_until_visible((By.XPATH, next_step)).click()
+    @print_timing("selenium_create_project:select_create_ework_project")
+    def measure():
+        page.wait_until_visible((By.XPATH, "//div[text()='Create a Single Task']/../..")).click()
+        page.wait_until_visible((By.XPATH, next_step)).click()
 
-        sub_measure()
+    measure()
 
-        ework_project_name = "EWork test" + ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    ework_project_name = "EWork test" + ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
-        @print_timing("selenium_create_project:fill_project_name")
-        def sub_measure():
-            page.wait_until_present((By.XPATH, "//input[@name='name']")).send_keys(ework_project_name)
-            page.wait_until_visible((By.XPATH, "//span[text()='Confirm and Continue']")).click()
+    page.wait_until_present((By.XPATH, "//input[@name='name']"))
 
-        sub_measure()
+    @print_timing("selenium_create_project:fill_project_name")
+    def measure():
+        page.wait_until_present((By.XPATH, "//input[@name='name']")).send_keys(ework_project_name)
+        page.wait_until_visible((By.XPATH, "//span[text()='Confirm and Continue']")).click()
 
-        @print_timing("selenium_create_project:fill_project_instructions")
-        def sub_measure():
-            page.wait_until_present((By.XPATH, "//trix-editor[@input='react_trix-editor_requirements']")).send_keys(
-                "Test description")
-            page.wait_until_visible((By.XPATH, "//input[@placeholder='Select Date']")).click()
-            page.wait_until_visible((By.XPATH, "//div[contains(@class,'calendar')]"))
-            page.wait_until_visible((By.XPATH, "//div[contains(@class,'sameDay')]")).click()
-            page.wait_until_visible((By.XPATH, next_step)).click()
+    measure()
 
-        sub_measure()
+    page.wait_until_present((By.XPATH, "//trix-editor[@input='react_trix-editor_requirements']"))
 
-        @print_timing("selenium_create_project:grant_access")
-        def sub_measure():
-            page.wait_until_visible((By.XPATH, "//input[@name='access_url']")).send_keys("https://www.google.com/")
-            page.wait_until_visible((By.XPATH, next_step)).click()
+    @print_timing("selenium_create_project:fill_project_instructions")
+    def measure():
+        page.wait_until_present((By.XPATH, "//trix-editor[@input='react_trix-editor_requirements']")).send_keys(
+            "Test description")
+        page.wait_until_visible((By.XPATH, "//input[@placeholder='Select Date']")).click()
+        page.wait_until_visible((By.XPATH, "//div[contains(@class,'calendar')]"))
+        page.wait_until_visible((By.XPATH, "//div[contains(@class,'sameDay')]")).click()
+        page.wait_until_visible((By.XPATH, next_step)).click()
 
-        sub_measure()
+    measure()
 
-        @print_timing("selenium_create_project:configure_team")
-        def sub_measure():
-            page.wait_until_visible((By.XPATH, "//input[@name='use_freelancers']")).click()
-            page.wait_until_visible((By.XPATH, "//div[contains(@class,'multiSelect')]")).click()
-            page.wait_until_visible((By.XPATH, "//input[@name='skill_ids']/following-sibling::div/input")).send_keys(
-                "AWK")
-            page.wait_until_visible((By.XPATH, "//div[@title='AWK']")).click()
-            page.wait_until_visible((By.XPATH, next_step)).click()
+    page.wait_until_visible((By.XPATH, "//input[@name='access_url']"))
 
-        sub_measure()
+    @print_timing("selenium_create_project:grant_access")
+    def measure():
+        page.wait_until_visible((By.XPATH, "//input[@name='access_url']")).send_keys("https://www.google.com/")
+        page.wait_until_visible((By.XPATH, next_step)).click()
 
-        @print_timing("selenium_create_project:payment")
-        def sub_measure():
-            page.wait_until_visible((By.XPATH, "//div[@type='redactor']/input")).send_keys("40")
-            page.wait_until_visible((By.XPATH, next_step)).click()
+    measure()
 
-        sub_measure()
+    page.wait_until_visible((By.XPATH, "//input[@name='use_freelancers']"))
 
-        @print_timing("selenium_create_project:publish_project")
-        def sub_measure():
-            page.wait_until_visible((By.XPATH, "//span[text()='Publish Project']")).click()
+    @print_timing("selenium_create_project:configure_team")
+    def measure():
+        page.wait_until_visible((By.XPATH, "//input[@name='use_freelancers']")).click()
+        page.wait_until_visible((By.XPATH, "//div[contains(@class,'multiSelect')]")).click()
+        page.wait_until_visible((By.XPATH, "//input[@name='skill_ids']/following-sibling::div/input")).send_keys(
+            "AWK")
+        page.wait_until_visible((By.XPATH, "//div[@title='AWK']")).click()
+        page.wait_until_visible((By.XPATH, next_step)).click()
 
-        sub_measure()
+    measure()
 
-        @print_timing("selenium_create_project:check_auto_comment")
-        def sub_measure():
-            actual_text = page.wait_until_visible((By.XPATH, "//div[contains(@class, 'singleTaskPreview')]/p")).text
-            expected_text = "We are already looking for engineers matching your requirements. You can check the project directly in EWORK."
-            assert actual_text == expected_text, f"Error: Expected '{expected_text}', but got '{actual_text}'"
+    page.wait_until_visible((By.XPATH, "//div[@type='redactor']/input"))
 
-        sub_measure()
+    @print_timing("selenium_create_project:payment")
+    def measure():
+        page.wait_until_visible((By.XPATH, "//div[@type='redactor']/input")).send_keys("40")
+        page.wait_until_visible((By.XPATH, next_step)).click()
+
+    measure()
+
+    page.wait_until_visible((By.XPATH, "//span[text()='Publish Project']"))
+
+    @print_timing("selenium_create_project:publish_project")
+    def measure():
+        page.wait_until_visible((By.XPATH, "//span[text()='Publish Project']")).click()
+
+    measure()
+
+    @print_timing("selenium_create_project:check_auto_comment")
+    def measure():
+        actual_text = page.wait_until_visible((By.XPATH, "//div[contains(@class, 'singleTaskPreview')]/p")).text
+        expected_text = "We are already looking for engineers matching your requirements. You can check the project directly in EWORK."
+        assert actual_text == expected_text, f"Error: Expected '{expected_text}', but got '{actual_text}'"
 
     measure()
 
@@ -170,43 +177,51 @@ def check_task_creation(webdriver):
 
     page = BasePage(webdriver)
 
-    @print_timing("selenium_create_task")
+    @print_timing("selenium_create_task:select_project_creation_option")
     def measure():
-        @print_timing("selenium_create_task:select_project_creation_option")
-        def sub_measure():
-            page.wait_until_present((By.XPATH, "//a[@class='issue-created-key issue-link']")).click()
-            page.wait_until_present((By.ID, "opsbar-operations_more")).click()
-            page.wait_until_visible((By.ID, create_project_option_id)).click()
+        page.wait_until_present((By.XPATH, "//a[@class='issue-created-key issue-link']")).click()
+        page.wait_until_present((By.ID, "opsbar-operations_more")).click()
+        page.wait_until_visible((By.ID, create_project_option_id)).click()
 
-        sub_measure()
+    measure()
 
-        next_step = "//span[text()='Continue']"
+    next_step = "//span[text()='Continue']"
 
-        @print_timing("selenium_create_task:select_create_ework_task")
-        def sub_measure():
-            page.wait_until_visible(
-                (By.XPATH, "//div[text()='Create a Task for an existing Stream Project']/../..")).click()
-            page.wait_until_visible((By.XPATH, next_step)).click()
+    @print_timing("selenium_create_task:select_create_ework_task")
+    def measure():
+        page.wait_until_visible(
+            (By.XPATH, "//div[text()='Create a Task for an existing Stream Project']/../..")).click()
+        page.wait_until_visible((By.XPATH, next_step)).click()
 
-        sub_measure()
+    measure()
 
-        ework_task_name = "EWork task" + ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    ework_task_name = "EWork task" + ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
-        @print_timing("selenium_create_task:fill_task_data")
-        def sub_measure():
-            page.wait_until_visible((By.XPATH, "//input[contains(@class,'selected')]")).send_keys(
-                ework_project_with_task)
-            page.wait_until_visible((By.XPATH, "//div[contains(@type,'redactor')]")).click()
-            page.wait_until_visible((By.XPATH, "//div[@type='dropdown']/div")).click()
-            page.wait_until_present((By.XPATH, "//input[@name='title']")).send_keys(ework_task_name)
-            page.wait_until_present((By.XPATH, "//trix-editor[@input='react_trix-editor_requirements']")).send_keys(
-                "Test description")
-            page.wait_until_visible((By.XPATH, "//input[@placeholder='Select Date']")).click()
-            page.wait_until_visible((By.XPATH, "//div[contains(@class,'calendar')]"))
-            page.wait_until_visible((By.XPATH, "//div[contains(@class,'sameDay')]")).click()
-            page.wait_until_visible((By.XPATH, "//span[text()='Create Task']")).click()
-            page.wait_until_visible((By.XPATH, "//h2[text()='Stream Task was published successfully']"))
+    @print_timing("selenium_create_task:select_ework_stream_project")
+    def measure():
+        page.wait_until_visible((By.XPATH, "//input[contains(@class,'selected')]")).send_keys(
+            ework_project_with_task)
+        page.wait_until_visible((By.XPATH, "//div[contains(@type,'redactor')]")).click()
+        page.wait_until_visible((By.XPATH, "//div[@type='dropdown']/div")).click()
 
-        sub_measure()
+    measure()
+
+    for k in range(number_of_attempts):
+        try:
+            page.wait_until_visible((By.XPATH, "//input[@name='title']"), small_wait_timeout)
+            break
+        except TimeoutException:
+            print('TimeoutException handled')
+
+    @print_timing("selenium_create_task:fill_task_data")
+    def measure():
+        page.wait_until_present((By.XPATH, "//input[@name='title']")).send_keys(ework_task_name)
+        page.wait_until_present((By.XPATH, "//trix-editor[@input='react_trix-editor_requirements']")).send_keys(
+            "Test description")
+        page.wait_until_visible((By.XPATH, "//input[@placeholder='Select Date']")).click()
+        page.wait_until_visible((By.XPATH, "//div[contains(@class,'calendar')]"))
+        page.wait_until_visible((By.XPATH, "//div[contains(@class,'sameDay')]")).click()
+        page.wait_until_visible((By.XPATH, "//span[text()='Create Task']")).click()
+        page.wait_until_visible((By.XPATH, "//h2[text()='Stream Task was published successfully']"))
 
     measure()
